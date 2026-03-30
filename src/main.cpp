ac040e6b7952484fd3ff74322e4a01c03139bd9c
Mirza Razaqan <mirzarazaqan@gmail.com>
2026-03-30 23:45:43 +0700

    Modify greeting to include user input
    Prompt user for their name and greet them.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main(int argc, char ** argv) {
-    cout << "Hello world!" << endl;
+    string nama ;
+    cout <<"silahkan masukkan nama anda";
+    getline(cin, nama);
+
+    cout << "Hello " << nama << endl;
 
     return 0;
 }
